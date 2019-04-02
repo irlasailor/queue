88f031c2d20ab94eb69794765cf9a6a4528a6520
irlasailor <48557774+irlasailor@users.noreply.github.com>
2019-04-02 11:22:15 +0300

    Работает, но криво

--- Main.cpp
+++ Main.cpp
@@ -5,24 +5,31 @@ using namespace std;
 
 int main() {
 	try{
-		Queue<int> a(8);
-		int* correct1 = new int(4);
-		int* correct2 = new int(8);
+		Queue<int> a(9);
+		/*int* correct1 = new int(4);
+		int* correct2 = new int(8);*/
 
 		a.enQueue(2);
-		/*a.enQueue(7);
+		a.enQueue(7);
 		a.enQueue(0);
 		a.enQueue(4);
 		a.enQueue(1);
 		a.enQueue(9);
 		a.enQueue(9);
-		a.enQueue(9);*/
+		a.enQueue(9);
 
-		correct1[0] = 2;
+		/*correct1[0] = 2;
 		correct1[1] = 7;
 		correct1[2] = 0;
-		correct1[3] = 4;
+		correct1[3] = 4;*/
 
+		cout << a.deQueue() << endl;
+		cout << a.deQueue() << endl;
+		cout << a.deQueue() << endl;
+		cout << a.deQueue() << endl;
+		cout << a.deQueue() << endl;
+		cout << a.deQueue() << endl;
+		cout << a.deQueue() << endl;
 		cout << a.deQueue() << endl;
 		/*a.deQueue();
 		a.deQueue();
@@ -34,19 +41,28 @@ int main() {
 		cout << "First test passed" << endl;*/
 
 		cout << "Huh very soon ^o^" << endl;
-		/*a.enQueue(5);
+		a.enQueue(5);
 		a.enQueue(9);
 		a.enQueue(2);
 		a.enQueue(8);
-*/
-		correct2[0] = 1;
+
+		cout << a.deQueue() << endl;
+		cout << a.deQueue() << endl;
+		cout << a.deQueue() << endl;
+		cout << a.deQueue() << endl;
+		cout << a.deQueue() << endl;
+		cout << a.deQueue() << endl;
+		cout << a.deQueue() << endl;
+		cout << a.deQueue() << endl;
+
+	/*	correct2[0] = 1;
 		correct2[1] = 9;
 		correct2[2] = 9;
 		correct2[3] = 9;
 		correct2[4] = 5;
 		correct2[5] = 9;
 		correct2[6] = 2;
-		correct2[7] = 8;
+		correct2[7] = 8;*/
 
 		/*for (int i = 0; i < 8; i++) {
 			assert(a.deQueue() == correct2[i]);
@@ -59,5 +75,6 @@ int main() {
 		cerr << error << endl;
 		return -1;
 	}
+	system("pause");
 	return 0;
 }
--- Queue.h
+++ Queue.h
@@ -1,5 +1,6 @@
 #ifndef QUEUE_H
 #define QUEUE_H
+#include <iostream>
 
 template <typename TypeData>
 class Queue
@@ -20,10 +21,10 @@ public:
 
 template <typename TypeData>
 Queue<TypeData>::Queue(int size) {
-	q_ = new TypeData[size];
+	q_ = new TypeData[size+1];
 	head_ = 1;
 	tail_ = 1;
-	size_ = size;
+	size_ = size+1;
 }
 
 template <typename TypeData>
@@ -38,10 +39,10 @@ void Queue<TypeData>::enQueue(TypeData i) {
 
 template <typename TypeData>
 TypeData Queue<TypeData>::deQueue() {
-	if (tail_ = head_ = 1) throw "Sorry, the queue is empty";
+	if (tail_ == head_ == 1) throw "Sorry, the queue is empty";
 	TypeData x = q_[head_];
 	if (head_ == size_) {
-		std::cout << "Queue overflow, make a circle" << endl;
+		std::cout << "Queue overflow, make a circle" << std::endl;
 		head_ = 1;
 	}
 	else {
